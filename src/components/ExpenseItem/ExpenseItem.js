5ebb0c932e59f8b45ec91a4987b540c1853ab47f
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2021-12-10 11:50:18 +0200

    Passing data via "props"

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-export function ExpenseItem() {
-  const expenseDate = new Date(2021, 12, 10);
-  const expenseItem = "Gym membership";
-  const expensePrice = 45;
-
+export function ExpenseItem({ title, amount, date }) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseItem}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
