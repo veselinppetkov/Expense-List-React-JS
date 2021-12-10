7330adc89a5cc869e5371e422f7970c038f9895c
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2021-12-10 11:59:28 +0200

    Adding "normal" JavaScript Logic to Components

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 export function ExpenseItem({ title, amount, date }) {
+  const month = date.toLocaleString(`en-us`, { month: `long` });
+  const day = date.toLocaleString(`en-us`, { day: `2-digit` });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
