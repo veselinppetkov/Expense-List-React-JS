95faf4ae23a8086417f6359eb56954816a5c593e
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2021-12-10 11:35:54 +0200

    Some changes made

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 export function ExpenseItem() {
+  const expenseDate = new Date(2021, 12, 10);
+  const expenseItem = "Gym membership";
+  const expensePrice = 45;
+
   return (
     <div className="expense-item">
-      <div>December 10th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Gym membership</h2>
-        <div className="expense-item__price">$45.00</div>
+        <h2>{expenseItem}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
