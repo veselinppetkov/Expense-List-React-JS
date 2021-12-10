e9dcdb19a56288dcc33746b42a3db9aea70c3b17
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2021-12-10 12:19:52 +0200

    Splitting components into Multiple Components

---
+++ src/components/ExpenseItem/ExpenseDate.js
@@ -0,0 +1,15 @@
+import "./ExpenseDate.css";
+
+export function ExpenseDate({ date }) {
+  const month = date.toLocaleString(`en-us`, { month: `long` });
+  const day = date.toLocaleString(`en-us`, { day: `2-digit` });
+  const year = date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
+    </div>
+  );
+}
