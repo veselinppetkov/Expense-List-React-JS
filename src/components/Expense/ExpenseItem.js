4ead55a3c5e1956c4cfb4ae949060ef94078e8c8
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2021-12-14 18:09:35 +0200

    Added Event Handler

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,12 +3,16 @@ import Card from "../UI/Card";
 import { ExpenseDate } from "./ExpenseDate";
 
 export function ExpenseItem({ title, amount, date }) {
+  const clickHandler = (e) => {
+    console.log(e.target.textContent);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
