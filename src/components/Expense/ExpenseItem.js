a6313a69ca4c3b915c78a3395abd74bca6d2a65c
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2021-12-14 18:27:24 +0200

    useState Hook in use

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,16 +1,18 @@
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import { useState } from "react";
 import { ExpenseDate } from "./ExpenseDate";
 
 export function ExpenseItem({ title, amount, date }) {
+  const [caption, setCaption] = useState(title);
   const clickHandler = (e) => {
-    console.log(e.target.textContent);
+    setCaption(`Updated!`);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{caption}</h2>
         <div className="expense-item__price">${amount}</div>
         <button onClick={clickHandler}>Change Title</button>
       </div>
